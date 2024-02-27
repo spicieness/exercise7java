e401a528efe40f6313fe618fbf7230303b38fe8d
Dylan A Olson 12th CCHS <117957@SWCSD.US>
2024-02-27 14:19:50 -0500

    Initial commit

---
+++ src/ContactInfo.java
@@ -0,0 +1,32 @@
+public class ContactInfo {
+
+    private String name;
+    private String email;
+
+    public ContactInfo(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void display() {
+        System.out.println("Name: "+name+" -- Email: "+email);
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ContactInfo contact1 = new ContactInfo("dyl", "email@email.com");
+        contact1.display();
+
+        BusinessContact contact2 = new BusinessContact("someone else", "otherpersonsemail@email.com", "4683628262");
+        contact2.display();
+    }
+}
